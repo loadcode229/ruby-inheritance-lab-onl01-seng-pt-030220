12130ea819bf32ff2051fc7a044068614421ee79
Adam Priest <sepone229@gmail.com>
2020-04-03 23:21:24 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,10 @@
 class User
-  attr_accessor :first, :last
-  def initialize(first, last)
-    @first, @last = first, last
+  attr_writer :first_name, :last_name
+  attr_reader :first_name, :last_name
+  def name=(first_name,last_name)
+    @first_name, @last_name = first_name, last_name
+  def name
+    @first_name
+    @last_name
   end
 end
