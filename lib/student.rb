7eab138ca9262adec46873a8e7139031302924f6
Adam Priest <sepone229@gmail.com>
2020-04-03 23:24:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,14 @@
 class Student < User
-  def initialize
-    @
+  attr_accessor :student
+  def initialize(student)
+    @student = student
+    @knowledge = []
+  end
+  def learn(student)
+    @knowledge << student
+  end
+  def knowledge
+    @@knowledge
+  end
+    
 end
